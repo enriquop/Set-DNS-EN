2c6316b8b0248960082bf5a76d741ac64b2b6424
enriquop <44776832+enriquop@users.noreply.github.com>
2020-11-30 17:38:41 +0100

    Update dnsgit.bat

--- dnsgit.bat
+++ dnsgit.bat
@@ -3,14 +3,14 @@ REM Created by: Enriquop
 REM SET DNS
 netsh interface ip show config
 pause > nul
-set /p Adaptador="Nombre de adapador: "
-set /p DNS="IP de DNS: "
+set /p Adaptador="Name of adapter: "
+set /p DNS="IP of DNS: "
 netsh interface ipv4 add dnsserver "%Adaptador%" address=%DNS% index=1
 echo listo!
 pause
 :start
 SET choice=
-SET /p choice=Deseas poner la dns alternativa? [Y/N]: 
+SET /p choice=Do you wish to set the alternative dns? [Y/N]: 
 IF NOT '%choice%'=='' SET choice=%choice:~0,1%
 IF '%choice%'=='Y' GOTO yes
 IF '%choice%'=='y' GOTO yes
@@ -23,16 +23,16 @@ GOTO start
 
 :no
 cls
-ECHO vas a salir del menu
+ECHO you are going to exit the menu
 timeout 2 > nul
-echo saliendo...
+echo exiting...
 timeout 1 > nul
 EXIT
 
 :yes
-set /p Adaptador1="Nombre de adapador: "
-set /p DNS2="IP de DNS: "
+set /p Adaptador1="Name of adapter: "
+set /p DNS2="IP of DNS: "
 netsh interface ipv4 add dnsserver "%Adaptador1%" address=%DNS2% index=2
-echo Ip alternativa asignada correctamente
+echo Alternative ip set correctly!!!
 timeout 2 > nul
 EXIT
